68c4515bd15086fc1e1aa37b0bc0b8774186344e
jawahar-717 <jawaharjawa2005@gmail.com>
2025-01-11 15:41:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from pyrogram import Client, filters
+  from pyrogram import Client, filters
 
 
-API_ID = ""
-API_HASH = ""
-BOT_TOKEN = ""
+API_ID = "27936813"
+API_HASH = "1a2c6cd63ebf1360b316a1179ae5d561"
+BOT_TOKEN = "MIIBCgKCAQEAyMEdY1aR+sCR3ZSJrtztKTKqigvO/vBfqACJLZtS7QMgCGXJ6XIR"
 
 TechMagazine = Client(
     name="samplebot",
@@ -12,6 +12,15 @@ TechMagazine = Client(
     bot_token=BOT_TOKEN
 )
 
+
+@TechMagazine.on_message(filter.command("start"))
+async def start_cmd(client, message):
+    print("START Command")
+
+
+@TechMagazine.on_message(filter.command("help"))
+async def help_cmd(client, message):
+    print("HELP Command")
 
 print("Bot was started")
 
