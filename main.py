66f11cb4b863503c3e51a13fe9233046f98be440
jawahar-717 <jawaharjawa2005@gmail.com>
2025-01-11 14:40:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+from pyrogram import Client, filters
 
+
+API_ID = ""
+API_HASH = ""
+BOT_TOKEN = ""
+
+TechMagazine = Client(
+    name="samplebot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+
+print("Bot was started")
+
+TechMagazine.run()
